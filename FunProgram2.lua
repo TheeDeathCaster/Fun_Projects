29b61e49f8baae3204fd11760690ff15ec905113
TheeDeathCaster <60681639+TheeDeathCaster@users.noreply.github.com>
2020-02-09 16:26:02 -0800

    Add files via upload

---
+++ FunProgram2.lua
@@ -0,0 +1,11 @@
+local Number1;
+local Number2;
+
+io.write("Input first number here: ");
+Number1 = tonumber(io.read());
+
+io.write("Input second number here: ");
+Number2 = tonumber(io.read());
+
+io.write(string.format("\n\nThe addition of %d + %d is %d", Number1, Number2, Number1 + Number2));
+io.read();
